79b8df2832eb9a1700be18a4080ea31246350751
gag0712 <gag0712@gmail.com>
2025-01-27 23:29:16 +0900

    Update RNCNaverMapGroundManagerSpec.kt

--- android/src/newarch/RNCNaverMapGroundManagerSpec.kt
+++ android/src/newarch/RNCNaverMapGroundManagerSpec.kt
@@ -8,12 +8,12 @@ import com.facebook.react.viewmanagers.RNCNaverMapGroundManagerInterface
 
 abstract class RNCNaverMapGroundManagerSpec<T : View> :
   SimpleViewManager<T>(),
-  RNCNaverMapGroundManagerInterface<T?> {
+  RNCNaverMapGroundManagerInterface<T> {
   private val mDelegate: ViewManagerDelegate<T>
 
   init {
     mDelegate = RNCNaverMapGroundManagerDelegate(this)
   }
 
-  override fun getDelegate(): ViewManagerDelegate<T>? = mDelegate
+  override fun getDelegate(): ViewManagerDelegate<T> = mDelegate
 }
